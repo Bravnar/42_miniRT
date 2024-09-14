3be839584a5ba4562202c8ae8bf511148e6b329e
hadri96 <68434862+hadri96@users.noreply.github.com>
2024-09-14 12:10:02 +0200

    Quick norminette fix + reorganization of shapes' helper functions

--- includes/cylinder.h
+++ includes/cylinder.h
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hmorand <hmorand@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/07 10:52:52 by hmorand           #+#    #+#             */
-/*   Updated: 2024/09/07 10:52:52 by hmorand          ###   ########.ch       */
+/*   Created: 2024/09/14 11:59:31 by hmorand           #+#    #+#             */
+/*   Updated: 2024/09/14 12:00:32 by hmorand          ###   ########.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 # include "object.h"
 
-typedef struct	s_cyl
+typedef struct s_cyl
 {
 	t_obj	shape;
 	double	diameter;
@@ -25,13 +25,21 @@ typedef struct	s_cyl
 	bool	closed;
 }	t_cyl;
 
-t_cyl	*cyl_create(char **cyl_line, int i);
-void	cyl_destroy(t_obj *shape);
-char	*get_name_cy(t_obj *shape);
-double	volume_cy(t_obj *shape);
-void	transform_cy(t_obj *shape, t_matrix transformation);
-t_inter	*local_intersect_cy(t_ray r, t_obj *cyl);
-t_cyl	*cylinder(int i);
-void	intersect_caps(t_obj *obj, t_ray r, t_inter *inter);
+t_cyl		*cyl_create(char **cyl_line, int i);
+t_cyl		*cylinder(int i);
+void		cyl_set_up(t_cyl *cyl, int i);
+void		cyl_destroy(t_obj *shape);
+char		*get_name_cy(t_obj *shape);
+double		volume_cy(t_obj *shape);
+t_inter		*local_intersect_cy(t_ray r, t_obj *cyl);
+t_tup		local_normal_at_cy(t_obj *cyl, t_tup point);
+
+/* CYLINDER SPECIFIC CALCULATIONS */
+
+t_tup		calculate_rotations(t_tup dir_vector);
+double		discriminant_cyl(t_obj *cyl, double *a, double *b);
+t_matrix	apply_transformation_cy(t_cyl *cyl);
+void		transform_cy(t_obj *shape, t_matrix transformation);
+void		intersect_caps(t_obj *obj, t_ray r, t_inter *inter);
 
 #endif
